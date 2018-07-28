dee9dd91ba18bf0cc156c5a0f42bcef7fc1ae6e0
sx515 <sxwr2013@live.com>
2018-07-29 02:37:47 +0800

    add new class source file CIAEngine.java

---
+++ app/src/main/java/pw/janyo/jycia/CIAEngine.java
@@ -0,0 +1,18 @@
+/*
+ *CIAEngine.java
+ *CIA 引擎类 提供数据采集 优化 转发
+ *DickLight
+ *2018-7-29
+ *
+ */
+package pw.janyo.jycia;
+
+/*
+ *CIAEngine
+ *引擎类 提供数据采集 优化 转发
+ *
+ */
+public class CIAEngine
+{
+	CIAEngine(){}
+}
