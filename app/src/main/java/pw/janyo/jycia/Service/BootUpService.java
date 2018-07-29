f367c17f85ebb7b2f3d29e5d2e89f0ee37ce1ecf
KamioMisuzu <506282668@qq.com>
2018-07-29 13:10:17 +0800

    add BootUpService

---
+++ app/src/main/java/pw/janyo/jycia/Service/BootUpService.java
@@ -0,0 +1,16 @@
+package pw.janyo.jycia.Service;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+
+public class BootUpService extends Service
+{
+
+	@Override
+	public IBinder onBind(Intent p1)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+}
